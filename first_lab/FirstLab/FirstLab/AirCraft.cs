2e74f8d1f2dea921b2a5cf88edbf9269e20979c4
trepixert <37483940+trepixert@users.noreply.github.com>
2018-11-09 16:42:09 +0400

    first labaratory of C#

---
+++ first_lab/FirstLab/FirstLab/AirCraft.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsArmorAirCraft
+{
+    class AirCraft
+    {
+        private float _startPosX;
+        private float _startPosY;
+        private int _pictureWidth;
+        private int _pictureHeight;
+        private const int AirCraftWidth = 100;
+        private const int AirCraftHeight = 100;
+        public int MaxSpeed { private set; get; }
+        public float Weight { private set; get; }
+        public Color MainColor { private set; get; }
+        public Color DopColor { private set; get; }
+
+
+        public AirCraft(int MaxSpeed, float Weight, Color MainColor, Color DopColor)
+        {
+            this.MaxSpeed = MaxSpeed;
+            this.Weight = Weight;
+            this.MainColor = MainColor;
+            this.DopColor = DopColor;
+
+        }
+
+        public void SetPosition(int x, int y, int width, int height)
+        {
+            _startPosX = x;
+            _startPosY = y;
+            _pictureWidth = width;
+            _pictureHeight = height;
+        }
+
+        public void moveAirCraft(Direction direction)
+        {
+            float step = MaxSpeed * 100 / Weight;
+            switch (direction)
+            {
+                case Direction.Right:
+                    if (_startPosX + step < _pictureWidth - AirCraftWidth)
+                        _startPosX += step;
+                    break;
+                case Direction.Left:
+                    if (_startPosX - step > 0)
+                        _startPosX -= step;
+                    break;
+                case Direction.Up:
+                    if (_startPosY - step > 0)
+                        _startPosY -= step;
+                    break;
+                case Direction.Down:
+                    if (_startPosY + step < _pictureHeight - AirCraftHeight)
+                        _startPosY += step;
+                    break;
+            }
+        }
+        public void DrawAirCraft(Graphics g)
+        {
+            Pen pen = new Pen(Color.Gray);
+            g.DrawEllipse(pen, _startPosX + 80, _startPosY + 50, 250, 50);
+            g.DrawEllipse(pen, _startPosX + 220, _startPosY-35, 80 , 220);
+            g.DrawEllipse(pen, _startPosX + 110, _startPosY+10, 30, 120);
+            Brush filler = new SolidBrush(DopColor);
+            g.FillEllipse(filler, _startPosX + 80, _startPosY + 50, 250, 50);
+            g.FillEllipse(filler, _startPosX + 220, _startPosY - 35, 80, 220);
+            g.FillEllipse(filler, _startPosX + 110, _startPosY + 10, 30, 120);
+        }
+    }
+}
---
+++ first_lab/FirstLab/FirstLab/FormAirCraft.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using WindowsArmorAirCraft;
+
+namespace WindowsArmorAirCraft
+{
+    public partial class FormAirCraft: Form
+    {
+        private AirCraft airCraft;
+        public FormAirCraft()
+        {
+            InitializeComponent();
+        }
+
+        private void Draw()
+        {
+            Bitmap bmp = new Bitmap(pictureAirCraft.Width, pictureAirCraft.Height);
+            Graphics gr = Graphics.FromImage(bmp);
+            airCraft.DrawAirCraft(gr);
+            pictureAirCraft.Image = bmp;
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void CreateButton_Click(object sender, EventArgs e)
+        {
+            Random rnd = new Random();
+            airCraft = new AirCraft(rnd.Next(1000, 3000), rnd.Next(5000, 10000), Color.Black, Color.Gray);
+            airCraft.SetPosition(rnd.Next(100, 300), rnd.Next(100, 300), pictureAirCraft.Width, pictureAirCraft.Height);
+            Draw();
+        }
+
+
+        private void pictureAirCraft_Click(object sender, EventArgs e)
+        {
+            string name = (sender as Button).Name;
+            switch (name)
+            {
+                case "Up":
+                    airCraft.moveAirCraft(Direction.Up);
+                    break;
+                case "Down":
+                    airCraft.moveAirCraft(Direction.Down);
+                    break;
+                case "ToLeft":
+                    airCraft.moveAirCraft(Direction.Left);
+                    break;
+                case "ToRight":
+                    airCraft.moveAirCraft(Direction.Right);
+                    break;
+            }
+            Draw();
+        }
+
+        
+    }
+}
