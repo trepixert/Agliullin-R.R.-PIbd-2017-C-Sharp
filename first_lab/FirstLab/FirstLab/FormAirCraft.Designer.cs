9d403678d14afd98d402d0d9c2812e260877a4a0
trepixert <37483940+trepixert@users.noreply.github.com>
2018-11-17 23:19:51 +0400

    Update FormAirCraft.Designer.cs

--- first_lab/FirstLab/FirstLab/FormAirCraft.Designer.cs
+++ first_lab/FirstLab/FirstLab/FormAirCraft.Designer.cs
@@ -34,7 +34,7 @@
             this.ToRight = new System.Windows.Forms.Button();
             this.Up = new System.Windows.Forms.Button();
             this.CreateButton = new System.Windows.Forms.Button();
-            this.button1 = new System.Windows.Forms.Button();
+            this.CreateCoolButton = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.pictureAirCraft)).BeginInit();
             this.SuspendLayout();
             // 
@@ -102,22 +102,22 @@
             this.CreateButton.UseVisualStyleBackColor = true;
             this.CreateButton.Click += new System.EventHandler(this.CreateButton_Click);
             // 
-            // button1
+            // CreateCoolButton
             // 
-            this.button1.Location = new System.Drawing.Point(81, 0);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 11;
-            this.button1.Text = "createCool";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.CreateCoolButton.Location = new System.Drawing.Point(81, 0);
+            this.CreateCoolButton.Name = "CreateCoolButton";
+            this.CreateCoolButton.Size = new System.Drawing.Size(75, 23);
+            this.CreateCoolButton.TabIndex = 11;
+            this.CreateCoolButton.Text = "createCool";
+            this.CreateCoolButton.UseVisualStyleBackColor = true;
+            this.CreateCoolButton.Click += new System.EventHandler(this.CreateCoolButton_Click);
             // 
             // FormAirCraft
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(884, 461);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.CreateCoolButton);
             this.Controls.Add(this.CreateButton);
             this.Controls.Add(this.Down);
             this.Controls.Add(this.ToLeft);
@@ -139,7 +139,7 @@
         private System.Windows.Forms.Button ToRight;
         private System.Windows.Forms.Button Up;
         private System.Windows.Forms.Button CreateButton;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button CreateCoolButton;
     }
 }
 
