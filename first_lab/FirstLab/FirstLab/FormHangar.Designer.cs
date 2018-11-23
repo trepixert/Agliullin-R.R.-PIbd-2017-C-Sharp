5cf3b52a8396161309f4e63fcd36e9bda861aa4d
trepixert <37483940+trepixert@users.noreply.github.com>
2018-11-24 01:05:46 +0400

    Update FormHangar.Designer.cs

--- first_lab/FirstLab/FirstLab/FormHangar.Designer.cs
+++ first_lab/FirstLab/FirstLab/FormHangar.Designer.cs
@@ -29,13 +29,13 @@
         private void InitializeComponent()
         {
             this.pictureBoxMain = new System.Windows.Forms.PictureBox();
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
+            this.buttonSetAirCraft = new System.Windows.Forms.Button();
+            this.buttonSetCoolAirCraft = new System.Windows.Forms.Button();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
-            this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
+            this.getAirCraftLabel = new System.Windows.Forms.Label();
+            this.place = new System.Windows.Forms.Label();
             this.maskedTextToGetIndex = new System.Windows.Forms.MaskedTextBox();
-            this.button3 = new System.Windows.Forms.Button();
+            this.buttonGetCar = new System.Windows.Forms.Button();
             this.pictureBoxRemoved = new System.Windows.Forms.PictureBox();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBoxMain)).BeginInit();
             this.groupBox1.SuspendLayout();
@@ -50,56 +50,56 @@
             this.pictureBoxMain.TabIndex = 0;
             this.pictureBoxMain.TabStop = false;
             // 
-            // button1
+            // buttonSetAirCraft
             // 
-            this.button1.Location = new System.Drawing.Point(760, 12);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(93, 41);
-            this.button1.TabIndex = 1;
-            this.button1.Text = "Припарковать самолет";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.buttonSetAirCraft_Click);
+            this.buttonSetAirCraft.Location = new System.Drawing.Point(760, 12);
+            this.buttonSetAirCraft.Name = "buttonSetAirCraft";
+            this.buttonSetAirCraft.Size = new System.Drawing.Size(93, 41);
+            this.buttonSetAirCraft.TabIndex = 1;
+            this.buttonSetAirCraft.Text = "Припарковать самолет";
+            this.buttonSetAirCraft.UseVisualStyleBackColor = true;
+            this.buttonSetAirCraft.Click += new System.EventHandler(this.buttonSetAirCraft_Click);
             // 
-            // button2
+            // buttonSetCoolAirCraft
             // 
-            this.button2.Location = new System.Drawing.Point(761, 59);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(93, 47);
-            this.button2.TabIndex = 2;
-            this.button2.Text = "Припарковать крутой самолет";
-            this.button2.UseVisualStyleBackColor = true;
-            this.button2.Click += new System.EventHandler(this.buttonSetCoolAirCraft_Click);
+            this.buttonSetCoolAirCraft.Location = new System.Drawing.Point(761, 59);
+            this.buttonSetCoolAirCraft.Name = "buttonSetCoolAirCraft";
+            this.buttonSetCoolAirCraft.Size = new System.Drawing.Size(93, 47);
+            this.buttonSetCoolAirCraft.TabIndex = 2;
+            this.buttonSetCoolAirCraft.Text = "Припарковать крутой самолет";
+            this.buttonSetCoolAirCraft.UseVisualStyleBackColor = true;
+            this.buttonSetCoolAirCraft.Click += new System.EventHandler(this.buttonSetCoolAirCraft_Click);
             // 
             // groupBox1
             // 
             this.groupBox1.Controls.Add(this.pictureBoxRemoved);
-            this.groupBox1.Controls.Add(this.button3);
+            this.groupBox1.Controls.Add(this.buttonGetCar);
             this.groupBox1.Controls.Add(this.maskedTextToGetIndex);
-            this.groupBox1.Controls.Add(this.label2);
-            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.place);
+            this.groupBox1.Controls.Add(this.getAirCraftLabel);
             this.groupBox1.Location = new System.Drawing.Point(608, 130);
             this.groupBox1.Name = "groupBox1";
             this.groupBox1.Size = new System.Drawing.Size(289, 332);
             this.groupBox1.TabIndex = 3;
             this.groupBox1.TabStop = false;
             // 
-            // label1
+            // getAirCraftLabel
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(18, 0);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(95, 13);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "Забрать самолет";
+            this.getAirCraftLabel.AutoSize = true;
+            this.getAirCraftLabel.Location = new System.Drawing.Point(18, 0);
+            this.getAirCraftLabel.Name = "getAirCraftLabel";
+            this.getAirCraftLabel.Size = new System.Drawing.Size(95, 13);
+            this.getAirCraftLabel.TabIndex = 0;
+            this.getAirCraftLabel.Text = "Забрать самолет";
             // 
-            // label2
+            // place
             // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(81, 22);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(42, 13);
-            this.label2.TabIndex = 1;
-            this.label2.Text = "Место:";
+            this.place.AutoSize = true;
+            this.place.Location = new System.Drawing.Point(81, 22);
+            this.place.Name = "place";
+            this.place.Size = new System.Drawing.Size(42, 13);
+            this.place.TabIndex = 1;
+            this.place.Text = "Место:";
             // 
             // maskedTextToGetIndex
             // 
@@ -108,15 +108,15 @@
             this.maskedTextToGetIndex.Size = new System.Drawing.Size(46, 20);
             this.maskedTextToGetIndex.TabIndex = 2;
             // 
-            // button3
+            // buttonGetCar
             // 
-            this.button3.Location = new System.Drawing.Point(181, 19);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(58, 24);
-            this.button3.TabIndex = 3;
-            this.button3.Text = "Забрать";
-            this.button3.UseVisualStyleBackColor = true;
-            this.button3.Click += new System.EventHandler(this.buttonGetCar_Click);
+            this.buttonGetCar.Location = new System.Drawing.Point(181, 19);
+            this.buttonGetCar.Name = "buttonGetCar";
+            this.buttonGetCar.Size = new System.Drawing.Size(58, 24);
+            this.buttonGetCar.TabIndex = 3;
+            this.buttonGetCar.Text = "Забрать";
+            this.buttonGetCar.UseVisualStyleBackColor = true;
+            this.buttonGetCar.Click += new System.EventHandler(this.buttonGetCar_Click);
             // 
             // pictureBoxRemoved
             // 
@@ -132,8 +132,8 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(884, 461);
             this.Controls.Add(this.groupBox1);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.buttonSetCoolAirCraft);
+            this.Controls.Add(this.buttonSetAirCraft);
             this.Controls.Add(this.pictureBoxMain);
             this.Name = "FormHangar";
             this.Text = "FormHangar";
@@ -148,13 +148,13 @@
         #endregion
 
         private System.Windows.Forms.PictureBox pictureBoxMain;
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button buttonSetAirCraft;
+        private System.Windows.Forms.Button buttonSetCoolAirCraft;
         private System.Windows.Forms.GroupBox groupBox1;
         private System.Windows.Forms.PictureBox pictureBoxRemoved;
-        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button buttonGetCar;
         private System.Windows.Forms.MaskedTextBox maskedTextToGetIndex;
-        private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label place;
+        private System.Windows.Forms.Label getAirCraftLabel;
     }
 }
